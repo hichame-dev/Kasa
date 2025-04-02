5197ade800b4780d0e161f7c502decb28937791c
hichame <shemsine@gmail.com>
2025-04-02 14:30:52 +0200

    Amélioration de la structure et des styles du Header et du Banner, ajout d'une classe main-container pour une meilleure mise en page, et ajustement des marges et des propriétés CSS pour une meilleure responsivité.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Layout from './components/Layout'
 
 function App() {
   return (
+    <div className="main-container">
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -14,6 +15,7 @@ function App() {
         <Route path="*" element={<Error404 />} />
       </Routes>
     </Layout>
+    </div>
   )
 }
 
